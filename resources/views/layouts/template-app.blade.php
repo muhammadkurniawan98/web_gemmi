7a1523dc1d503899ed3ea3edf2229cd56f3f7b41
muhammadkurniawan98 <muhammadadekurniawan98@gmail.com>
2021-07-16 06:09:44 +0700

    update-4a

--- resources/views/layouts/template-app.blade.php
+++ resources/views/layouts/template-app.blade.php
@@ -80,5 +80,19 @@
 <!-- Template Main JS File -->
 <script src="{{asset('assets/js/main.js')}}"></script>
 
+<script>
+    $('#submitBtn').click(function() {
+        /* when the button in the form, display the entered values in the modal */
+        $('#lname').text($('#lastname').val());
+        $('#fname').text($('#firstname').val());
+    });
+
+    $('#submit').click(function(){
+        /* when the submit button in the modal is clicked, submit the form */
+        alert('submitting');
+        $('#formfield').submit();
+    });
+</script>
+
 </body>
 </html>
